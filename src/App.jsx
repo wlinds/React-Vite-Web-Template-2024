0b35e56fe7587062f3455aaf9d1aca07ffc99634
wlinds <w.lindstedt@gmail.com>
2024-11-02 00:27:39 +0100

    add /terms and /policy, add auth styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { TermsPage, PrivacyPage } from './components/Legal';
 import PortalLanding from './components/PortalLanding'
 import Footer from './components/Footer';
 
-function App() {
-  return (
-    <>
-    <PortalLanding />
-    <Footer />
-    </>
-  )
-}
-
-export default App
+const App = () => {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route 
+            path="/" 
+            element={
+              <>
+                <PortalLanding />
+                <Footer />
+              </>
+            } 
+          />
+          <Route path="/terms" element={<TermsPage />} />
+          <Route path="/policy" element={<PrivacyPage />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  };
+  
+  export default App;
